e758061b1c4a3b42d939f9df0b5d773ddcabb511
DAVIDLINUX <dtoscano461@gmail.com>
2022-07-03 19:19:07 -0500

    este archivo ya me deja buscar con explorador

--- alarma.py
+++ alarma.py
@@ -2,10 +2,19 @@ from pydub import AudioSegment
 from pydub.playback import play
 from playsound import playsound
 
+
 def run():
+    import tkinter as tk
+
+    from tkinter import filedialog
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()    
+    
     ruta = '/home/linux-t/Music/still_loving_you.mp3'
-    print("jejeje")
     playsound(ruta)
+    
 
 
 if __name__=='__main__':
