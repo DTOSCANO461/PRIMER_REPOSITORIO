45bc4ef09c59d109457de8101affe112613fa1b5
DAVIDLINUX <dtoscano461@gmail.com>
2022-07-03 20:10:08 -0500

    acabamos de usar widgets basicos, se declara y se usa pack, ahora vamos a meter botones con funciones

--- alarma.py
+++ alarma.py
@@ -1,12 +1,52 @@
-from playsound import playsound
+# EN ESTE DOCUMENTO EXPLORAMOS EL USO DE TKINTER PARA HACER UNA ALARMA 
+# EL VIDEO ESTA BASADO EN https://www.youtube.com/watch?v=jqRHhWjKDD8
+
+
+from playsound import playsound# playsound es el comando para reproducir musica
+# no tiene nada que ver con tkinter que es dedicado a la parte grafica nada mas 
+
+
 import tkinter 
+#importamos tkinter... no ponemos nada de as tk
+
 from tkinter import filedialog
 
 
 def run():
 
-    root = tkinter.Tk()
-    root.withdraw()
+    ventana = tkinter.Tk()#creamos la ventana que va a contener todo lo necesario 
+    ventana.geometry("300x300")# definimos el tamaño de la pantall
+    
+    
+    # un gidget es un objeto basicamente, etiqueta, boton.. etc 
+    etiqueta = tkinter.Label(ventana, text = "hola mundo!")
+    etiqueta = tkinter.Label(ventana, text = "hola mundo!", bg = "gray")
+    #bg significa back ground
+    
+    #PACK pone la etiqueta en la ventana con los detalles que pongas, colores etc 
+    
+    etiqueta.pack()#el metodo pack es el que te deja poner la etiqueta descrita 
+    #pone todo en el centro sin mucha confioguracion
+    etiqueta.pack(side = tkinter.RIGHT)# por ejemplo ahi movemos el mensaje a la derecha 
+    # pack admite diferentes parametros 
+    etiqueta.pack(side = tkinter.TOP, fill = tkinter.X)# asi podemos hacer que la etiqueta se "alarge"
+   
+    boton1 =tkinter.Button(ventana, text = "guardar")
+    boton1.pack()
+   
+   
+   
+   
+   
+    ventana.mainloop()#emepzamos a ejecutar un hilo queeste en loop monitoreando la ventana
+   
+    
+    
+    
+    
+    
+    
+    ventana.withdraw()
     file_path = filedialog.askopenfilename()
     playsound(file_path)
     
