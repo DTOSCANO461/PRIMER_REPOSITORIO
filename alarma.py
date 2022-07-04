23917cd18e06edb4ab1ebccf697b321eaf41d164
DAVIDLINUX <dtoscano461@gmail.com>
2022-07-03 19:42:18 -0500

    usamos de forma basica tkinter, pero ahora si vamos a enteder el codigo

--- alarma.py
+++ alarma.py
@@ -1,19 +1,14 @@
-from pydub import AudioSegment
-from pydub.playback import play
 from playsound import playsound
+import tkinter 
+from tkinter import filedialog
 
 
 def run():
-    import tkinter as tk
 
-    from tkinter import filedialog
-    root = tk.Tk()
+    root = tkinter.Tk()
     root.withdraw()
-
-    file_path = filedialog.askopenfilename()    
-    
-    ruta = '/home/linux-t/Music/still_loving_you.mp3'
-    playsound(ruta)
+    file_path = filedialog.askopenfilename()
+    playsound(file_path)
     
 
 
